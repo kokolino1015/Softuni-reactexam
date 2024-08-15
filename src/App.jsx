41779e2975765f91193195f468d4ebe45a59397a
Kaloyan Nedelkov <nedelkovkaloian@gmail.com>
2024-08-15 23:53:14 +0300

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
+import Login from './components/login/Login'
+import Register from './components/register/Register'
+import CreateBook from './components/createBook/CreateBook'
+import BookList from './components/bookList/BookList'
+import BookDetails from './components/bookDetails/BookDetails'
+import CreateComment from './components/commentCreate/CommentCreate'
 
 function App() {
   return (
     <>
       <div id="box">
         <main id="main-content">
-          <Header />
+          <Header />    
           <Routes>
-            <Route path='/' element={<Home />}></Route>
+            <Route path='/' element={<Home />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/register' element={<Register />}/>
+            <Route path='/book/create' element={<CreateBook />}/>
+            <Route path='/book/:bookId' element={<BookDetails />}/>
+            <Route path='/book/list' element={<BookList />}/>
+            <Route path='/book/:bookId/comment' element={<CreateComment />}/>
           </Routes>
         </main>
       </div>
